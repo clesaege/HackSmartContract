8624040a24aac6a8a42c66064bf6f91f57d15c67
clesaege <clemage@live.fr>
2017-04-22 14:30:53 +0200

    Create 10_HeadTail.sol

---
+++ 10_HeadTail.sol
@@ -0,0 +1,56 @@
+// Hidden head or tail.
+// If the party B guess the choice of party A, it wins 2 ethers, otherwise party A does.
+contract HeadTail {
+    address partyA;
+    address partyB;
+    bytes32 commitmentA;
+    bool chooseHeadB;
+    uint timeB;
+    
+    
+    
+    /** @dev Constructor, commit head or tail.
+     *  @param co_mmitmentA is keccak256(chooseHead,randomNumber);
+     */
+    function HeadTail(bytes32 _commitmentA) payable {
+        require(msg.value == 1 ether);
+        
+        commitmentA=_commitmentA;
+        partyA=msg.sender;
+    }
+    
+    /** @dev Guess the choice of party A.
+     *  @param _chooseHead True if the guess is head, false otherwize.
+     */
+    function guess(bool _chooseHead) payable {
+        require(msg.value == 1 ether);
+        
+        chooseHeadB=_chooseHead;
+        timeB=now;
+    }
+    
+    /** @dev Reveal the commited value and send ETH to the winner.
+     *  @param _chooseHead True if head was chose.
+     *  @param _randomNumber The random number chosen to obfuscate the commitment.
+     */
+    function resolve(bool _chooseHead, uint _randomNumber) {
+        require(msg.sender == partyA);
+        require(keccak256(_chooseHead, _randomNumber) == commitmentA);
+        require(this.balance >= 2 ether);
+        
+        if (_chooseHead == chooseHeadB)
+            partyB.transfer(2 ether);
+        else
+            partyA.transfer(2 ether);
+    }
+    
+    /** @dev Time out party A if it takes more than 1 day to reveal.
+     *  Send ETH to party B.
+     * */
+    function timeOut() {
+        require(now > timeB + 1 days);
+        require(this.balance>=2 ether);
+        partyB.transfer(2 ether);
+    }
+    
+}
