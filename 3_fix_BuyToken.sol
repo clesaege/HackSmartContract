c1c88ea5731d38bd6180d53185680875b488eeaf
Quentin Gaultier <quentin.gaultier@gmail.com>
2017-04-22 17:14:12 +0200

    try fix contract 3

--- 3_fix_BuyToken.sol
+++ 3_fix_BuyToken.sol
@@ -13,7 +13,12 @@ contract BuyToken {
      */
     function buyVotes(uint _amount, uint _price) payable {
         require(_price>=price); // The price is at least the current price.
-        require(_price* _amount * 1 ether >= msg.value); // You have paid at least the total price.
+        
+        require(_amount > 0);
+        require(_price * _amount / _amount == _price);
+        require(_price * _amount * 1 ether /  1 ether == _price * _amount);
+        
+        require(_price * _amount * 1 ether <= msg.value); // You have paid at least the total price.
         balances[msg.sender]+=_amount;
     }
 
