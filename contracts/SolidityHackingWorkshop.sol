7ded81fa48e4b5c4f3d81d0775fddc1dc21ec5eb
Clément Lesaege <clemage@live.fr>
2017-08-05 22:01:56 +0200

    Update SolidityHackingWorkshop.sol

--- contracts/SolidityHackingWorkshop.sol
+++ contracts/SolidityHackingWorkshop.sol
@@ -69,7 +69,7 @@ contract BuyToken {
      *  @param _amount The amount to buy.
      *  @param _price  The maximum price to buy those in ETH.
      */
-    function buyVotes(uint _amount, uint _price) payable {
+    function buyToken(uint _amount, uint _price) payable {
         require(_price>=price); // The price is at least the current price.
         require(_price* _amount * 1 ether >= msg.value); // You have paid at least the total price.
         balances[msg.sender]+=_amount;
