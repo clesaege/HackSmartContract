c07da46367133a3a7aaff44bba6b82bb6be24109
Clément Lesaege <clemage@live.fr>
2018-08-03 13:27:31 +0100

    Remove Ex11 and put bonus instead;

--- contracts/SolidityHackingWorkshop.sol
+++ contracts/SolidityHackingWorkshop.sol
@@ -312,31 +312,7 @@ contract HeadTail {
     
 }
 
-//*** Exercice 11 ***//
-// You can store ETH in this contract and redeem them.
-contract VaultInvariant {
-    mapping(address => uint) public balances;
-    uint totalBalance;
+//*** Exercice Bonus ***//
+// One of the previous contracts has 2 vulnerabilities.
+// Find which one and describe the second vulnerability.
 
-    /// @dev Store ETH in the contract.
-    function store() payable {
-        balances[msg.sender]+=msg.value;
-        totalBalance+=msg.value;
-    }
-    
-    /// @dev Redeem your ETH.
-    function redeem() {
-        uint toTranfer = balances[msg.sender];
-        msg.sender.transfer(toTranfer);
-        balances[msg.sender]=0;
-        totalBalance-=toTranfer;
-    }
-    
-    /// @dev Let a user get all funds if an invariant is broken.
-    function invariantBroken() {
-        require(totalBalance!=this.balance);
-        
-        msg.sender.transfer(this.balance);
-    }
-    
-}
