53b3abe2d4e3fc74eb9cef03e05b5204014d8d98
Shaleen Jain <shaleen@jain.sh>
2021-03-04 14:56:20 +0530

    update to solc v0.8

--- contracts/SolidityHackingWorkshop.sol
+++ contracts/SolidityHackingWorkshop.sol
@@ -10,14 +10,15 @@ You are required to find vulnerabilities where an attacker harms someone else.
 Being able to destroy your own stuff is not a vulnerability and should be dealt at the interface level.
 */
 
-pragma solidity ^0.4.10;
+pragma solidity ^0.8.2;
+
 //*** Exercice 1 ***//
 // Simple token you can buy and send.
-contract SimpleToken{
+contract SimpleToken {
     mapping(address => uint) public balances;
     
     /// @dev Buy token at the price of 1ETH/token.
-    function buyToken() payable {
+    function buyToken() public payable {
         balances[msg.sender]+=msg.value / 1 ether;
     }
     
@@ -25,7 +26,7 @@ contract SimpleToken{
      *  @param _recipient The recipient.
      *  @param _amount The amount to send.
      */
-    function sendToken(address _recipient, uint _amount) {
+    function sendToken(address _recipient, uint _amount) public {
         require(balances[msg.sender]!=0); // You must have some tokens.
         
         balances[msg.sender]-=_amount;
@@ -35,60 +36,6 @@ contract SimpleToken{
 }
 
 //*** Exercice 2 ***//
-// You can buy voting rights by sending ether to the contract.
-// You can vote for the value of your choice.
-contract VoteTwoChoices{
-    mapping(address => uint) public votingRights;
-    mapping(address => uint) public votesCast;
-    mapping(bytes32 => uint) public votesReceived;
-    
-    /// @dev Get 1 voting right per ETH sent.
-    function buyVotingRights() payable {
-        votingRights[msg.sender]+=msg.value/(1 ether);
-    }
-    
-    /** @dev Vote with nbVotes for a proposition.
-     *  @param _nbVotes The number of votes to cast.
-     *  @param _proposition The proposition to vote for.
-     */
-    function vote(uint _nbVotes, bytes32 _proposition) {
-        require(_nbVotes + votesCast[msg.sender]<=votingRights[msg.sender]); // Check you have enough voting rights.
-        
-        votesCast[msg.sender]+=_nbVotes;
-        votesReceived[_proposition]+=_nbVotes;
-    }
-
-}
-
-//*** Exercice 3 ***//
-// You can buy tokens.
-// The owner can set the price.
-contract BuyToken {
-    mapping(address => uint) public balances;
-    uint public price=1;
-    address public owner=msg.sender;
-    
-    /** @dev Buy tokens.
-     *  @param _amount The amount to buy.
-     *  @param _price  The price to buy those in ETH.
-     */
-    function buyToken(uint _amount, uint _price) payable {
-        require(_price>=price); // The price is at least the current price.
-        require(_price * _amount * 1 ether <= msg.value); // You have paid at least the total price.
-        balances[msg.sender]+=_amount;
-    }
-    
-    /** @dev Set the price, only the owner can do it.
-     *  @param _price The new price.
-     */
-    function setPrice(uint _price) {
-        require(msg.sender==owner);
-        
-        price=_price;
-    }
-}
-
-//*** Exercice 4 ***//
 // Contract to store and redeem money.
 contract Store {
     struct Safe {
@@ -99,16 +46,16 @@ contract Store {
     Safe[] public safes;
     
     /// @dev Store some ETH.
-    function store() payable {
+    function store() public payable {
         safes.push(Safe({owner: msg.sender, amount: msg.value}));
     }
     
     /// @dev Take back all the amount stored.
-    function take() {
+    function take() public {
         for (uint i; i<safes.length; ++i) {
-            Safe safe = safes[i];
+            Safe storage safe = safes[i];
             if (safe.owner==msg.sender && safe.amount!=0) {
-                msg.sender.transfer(safe.amount);
+                payable(msg.sender).transfer(safe.amount);
                 safe.amount=0;
             }
         }
@@ -116,66 +63,7 @@ contract Store {
     }
 }
 
-//*** Exercice 5 ***//
-// Count the total contribution of each user.
-// Assume that the one creating the contract contributed 1ETH.
-contract CountContribution{
-    mapping(address => uint) public contribution;
-    uint public totalContributions;
-    address owner=msg.sender;
-    
-    /// @dev Constructor, count a contribution of 1 ETH to the creator.
-    function CountContribution() public {
-        recordContribution(owner, 1 ether);
-    }
-    
-    /// @dev Contribute and record the contribution.
-    function contribute() public payable {
-        recordContribution(msg.sender, msg.value);
-    }
-    
-    /** @dev Record a contribution. To be called by CountContribution and contribute.
-     *  @param _user The user who contributed.
-     *  @param _amount The amount of the contribution.
-     */
-    function recordContribution(address _user, uint _amount) {
-        contribution[_user]+=_amount;
-        totalContributions+=_amount;
-    }
-    
-}
-
-//*** Exercice 6 ***//
-contract Token {
-    mapping(address => uint) public balances;
-    
-    /// @dev Buy token at the price of 1ETH/token.
-    function buyToken() payable {
-        balances[msg.sender]+=msg.value / 1 ether;
-    }
-    
-    /** @dev Send token.
-     *  @param _recipient The recipient.
-     *  @param _amount The amount to send.
-     */
-    function sendToken(address _recipient, uint _amount) {
-        require(balances[msg.sender]>=_amount); // You must have some tokens.
-        
-        balances[msg.sender]-=_amount;
-        balances[_recipient]+=_amount;
-    }
-    
-    /** @dev Send all tokens.
-     *  @param _recipient The recipient.
-     */
-    function sendAllTokens(address _recipient) {
-        balances[_recipient]=+balances[msg.sender];
-        balances[msg.sender]=0;
-    }
-    
-}
-
-//*** Exercice 7 ***//
+//*** Exercice 3 ***//
 // You can buy some object.
 // Further purchases are discounted.
 // You need to pay basePrice / (1 + objectBought), where objectBought is the number of object you previously bought.
@@ -184,7 +72,7 @@ contract DiscountedBuy {
     mapping (address => uint) public objectBought;
 
     /// @dev Buy an object.
-    function buy() payable {
+    function buy() public payable {
         require(msg.value * (1 + objectBought[msg.sender]) == basePrice);
         objectBought[msg.sender]+=1;
     }
@@ -192,41 +80,41 @@ contract DiscountedBuy {
     /** @dev Return the price you'll need to pay.
      *  @return price The amount you need to pay in wei.
      */
-    function price() constant returns(uint price) {
+    function price() public view returns (uint) {
         return basePrice/(1 + objectBought[msg.sender]);
     }
     
 }
 
-//*** Exercice 8 ***//
+//*** Exercice 4 ***//
 // You choose Head or Tail and send 1 ETH.
 // The next party send 1 ETH and try to guess what you chose.
 // If it succeed it gets 2 ETH, else you get 2 ETH.
 contract HeadOrTail {
     bool public chosen; // True if head/tail has been chosen.
     bool lastChoiceHead; // True if the choice is head.
-    address public lastParty; // The last party who chose.
+    address payable public lastParty; // The last party who chose.
     
     /** @dev Must be sent 1 ETH.
      *  Choose head or tail to be guessed by the other player.
      *  @param _chooseHead True if head was chosen, false if tail was chosen.
      */
-    function choose(bool _chooseHead) payable {
+    function choose(bool _chooseHead) public payable {
         require(!chosen);
         require(msg.value == 1 ether);
         
         chosen=true;
         lastChoiceHead=_chooseHead;
-        lastParty=msg.sender;
+        lastParty=payable(msg.sender);
     }
     
     
-    function guess(bool _guessHead) payable {
+    function guess(bool _guessHead) public payable {
         require(chosen);
         require(msg.value == 1 ether);
         
         if (_guessHead == lastChoiceHead)
-            msg.sender.transfer(2 ether);
+            payable(msg.sender).transfer(2 ether);
         else
             lastParty.transfer(2 ether);
             
@@ -234,30 +122,30 @@ contract HeadOrTail {
     }
 }
 
-//*** Exercice 9 ***//
+//*** Exercice 5 ***//
 // You can store ETH in this contract and redeem them.
 contract Vault {
     mapping(address => uint) public balances;
 
     /// @dev Store ETH in the contract.
-    function store() payable {
+    function store() public payable {
         balances[msg.sender]+=msg.value;
     }
     
     /// @dev Redeem your ETH.
-    function redeem() {
-        msg.sender.call.value(balances[msg.sender])();
+    function redeem() public {
+        msg.sender.call{ value: balances[msg.sender] }("");
         balances[msg.sender]=0;
     }
 }
 
-//*** Exercice 10 ***//
+//*** Exercice 6 ***//
 // You choose Head or Tail and send 1 ETH.
 // The next party send 1 ETH and try to guess what you chose.
 // If it succeed it gets 2 ETH, else you get 2 ETH.
 contract HeadTail {
-    address public partyA;
-    address public partyB;
+    address payable public partyA;
+    address payable public partyB;
     bytes32 public commitmentA;
     bool public chooseHeadB;
     uint public timeB;
@@ -267,33 +155,33 @@ contract HeadTail {
     /** @dev Constructor, commit head or tail.
      *  @param _commitmentA is keccak256(chooseHead,randomNumber);
      */
-    function HeadTail(bytes32 _commitmentA) payable {
+    constructor(bytes32 _commitmentA) payable {
         require(msg.value == 1 ether);
         
         commitmentA=_commitmentA;
-        partyA=msg.sender;
+        partyA=payable(msg.sender);
     }
     
     /** @dev Guess the choice of party A.
      *  @param _chooseHead True if the guess is head, false otherwize.
      */
-    function guess(bool _chooseHead) payable {
+    function guess(bool _chooseHead) public payable {
         require(msg.value == 1 ether);
         require(partyB==address(0));
         
         chooseHeadB=_chooseHead;
-        timeB=now;
-        partyB=msg.sender;
+        timeB=block.timestamp;
+        partyB=payable(msg.sender);
     }
     
     /** @dev Reveal the commited value and send ETH to the winner.
      *  @param _chooseHead True if head was chosen.
      *  @param _randomNumber The random number chosen to obfuscate the commitment.
      */
-    function resolve(bool _chooseHead, uint _randomNumber) {
+    function resolve(bool _chooseHead, uint _randomNumber) public {
         require(msg.sender == partyA);
-        require(keccak256(_chooseHead, _randomNumber) == commitmentA);
-        require(this.balance >= 2 ether);
+        require(keccak256(abi.encodePacked(_chooseHead, _randomNumber)) == commitmentA);
+        require(address(this).balance >= 2 ether);
         
         if (_chooseHead == chooseHeadB)
             partyB.transfer(2 ether);
@@ -304,45 +192,10 @@ contract HeadTail {
     /** @dev Time out party A if it takes more than 1 day to reveal.
      *  Send ETH to party B.
      * */
-    function timeOut() {
-        require(now > timeB + 1 days);
-        require(this.balance>=2 ether);
+    function timeOut() public {
+        require(block.timestamp > timeB + 1 days);
+        require(address(this).balance >= 2 ether);
         partyB.transfer(2 ether);
-    }
-}
-
-//*** Exercice 11 ***//
-// You can create coffers put money into it and withdraw it.
-contract Coffers {
-    struct Coffer {address owner; uint[] slots;}
-    Coffer[] public coffers;
-    
-    /** @dev Create coffers.
-     *  @param _slots The amount of slots the coffer will have.
-     * */
-    function createCoffer(uint _slots) external {
-        Coffer storage coffer = coffers[coffers.length++];
-        coffer.owner = msg.sender;
-        coffer.slots.length = _slots;
-    }
-    
-    /** @dev Deposit money in one's coffer slot.
-     *  @dev _coffer The coffer to deposit money on.
-     *  @param _slot The slot to deposit money.
-     * */
-    function deposit(uint _coffer, uint _slot) payable external {
-        Coffer storage coffer = coffers[_coffer];
-        coffer.slots[_slot] += msg.value;
-    }
-    
-    /** @dev withdraw all of the money of one's coffer slot.
-     *  @param _slot The slot to withdraw money from.
-     * */
-    function withdraw(uint _coffer, uint _slot) external {
-        Coffer storage coffer = coffers[_coffer];
-        require(coffer.owner == msg.sender);
-        msg.sender.transfer(coffer.slots[_slot]);
-        coffer.slots[_slot] = 0;
     }
 }
 
