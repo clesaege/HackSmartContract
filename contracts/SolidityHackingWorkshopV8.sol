06fcf2e713f973fa68f48c897f66dcff9477118a
MerlinEgalite <egalite.merlin@gmail.com>
2021-03-08 17:38:37 +0000

    docs(SolidityHackingWorkshopV8): add some comments

--- contracts/SolidityHackingWorkshopV8.sol
+++ contracts/SolidityHackingWorkshopV8.sol
@@ -124,17 +124,20 @@ contract Vault {
 // The owner can deposit 1 ETH whenever he wants.
 // He can only withdraw when the deposited amount reaches 10 ETH.
 contract PiggyBank {
-    
+
     address owner;
-    
+
+    /// @dev Set msg.sender as owner
     constructor() {
         owner = msg.sender;
     }
-    
+
+    /// @dev Deposit 1 ETH in the smart contract
     function deposit() public payable {
         require(msg.sender == owner && msg.value == 1 ether && address(this).balance <= 10 ether);
     }
-    
+
+    /// @dev Withdraw the entire smart contract balance
     function withdrawAll() public {
         require(msg.sender == owner && address(this).balance == 10 ether);
         payable(owner).send(address(this).balance);
